bcc2de2314d213a217a8f8dd43c8c8298c7e6879
Lakshay <lakshayf9@gmail.com>
2025-12-30 00:23:37 +0530

    Added Input for Console

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-   std::cout << "$ ";
+  std::cout << "$ ";
+
+  std::string command = "";
+  std::getline(std::cin, command);
+
+  bool found = false;
+
+  if (!found)
+    std::cout << command << ": command not found" << std::endl;
 }
