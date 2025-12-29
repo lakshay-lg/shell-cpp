6d27d2a7a64e3a54ad329b43a026d3334c33cfd7
Lakshay <lakshayf9@gmail.com>
2025-12-30 01:04:07 +0530

    Implemented echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 #include <string>
 
 int main()
@@ -11,12 +13,31 @@ int main()
   {
     // TODO: Uncomment the code below to pass the first stage
     std::cout << "$ ";
+    bool found = false;
 
     std::string command = "";
     std::getline(std::cin, command);
-    if (command == "exit")
+
+    std::vector<std::string> commands;
+    std::stringstream ss(command);
+    std::string temp;
+
+    while (getline(ss, temp, ' '))
+    {
+      commands.push_back(temp);
+    }
+
+    if (commands[0] == "exit")
       break;
-    bool found = false;
+    if (commands[0] == "echo")
+    {
+      found = true;
+      for (int i = 1; i < commands.size(); i++)
+      {
+        std::cout << commands[i] << " ";
+      }
+      std::cout << std::endl;
+    }
 
     if (!found)
       std::cout << command << ": command not found" << std::endl;
