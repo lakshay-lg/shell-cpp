681f79f56f6efba3d6d62fcd7ac7ab521400b290
Lakshay <lakshayf9@gmail.com>
2025-12-30 01:13:13 +0530

    Implemented type command

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <vector>
 #include <string>
+#include <unordered_set>
 
 int main()
 {
@@ -13,25 +14,33 @@ int main()
   {
     // TODO: Uncomment the code below to pass the first stage
     std::cout << "$ ";
-    bool found = false;
 
     std::string command = "";
     std::getline(std::cin, command);
 
+    // checking the operation in the command
     std::vector<std::string> commands;
     std::stringstream ss(command);
     std::string temp;
+
+    // tracking available commands
+    std::unordered_set<std::string> availableCommands;
+    availableCommands.insert("echo");
+    availableCommands.insert("exit");
+    availableCommands.insert("type");
 
     while (getline(ss, temp, ' '))
     {
       commands.push_back(temp);
     }
 
+    // EXIT
     if (commands[0] == "exit")
       break;
+
+    // ECHO
     if (commands[0] == "echo")
     {
-      found = true;
       for (int i = 1; i < commands.size(); i++)
       {
         std::cout << commands[i] << " ";
@@ -39,7 +48,23 @@ int main()
       std::cout << std::endl;
     }
 
-    if (!found)
-      std::cout << command << ": command not found" << std::endl;
+    // TYPE
+    if (commands[0] == "type")
+    {
+      for (int i = 1; i < commands.size(); i++)
+      {
+        if (availableCommands.count(commands[i]))
+        {
+          std::cout << commands[i] << " is a shell builtin" << std::endl;
+        }
+        else
+        {
+          std::cout << commands[i] << ": not found" << std::endl;
+        }
+      }
+    }
+
+    if (!availableCommands.count(commands[0]))
+      std::cout << commands[0] << ": command not found" << std::endl;
   }
 }
